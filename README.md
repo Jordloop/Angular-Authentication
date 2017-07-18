244078bef50501034b5e87bcace441bc0509fdfb
John Dowd and Jordan Loop <john+jordan@>
2017-07-18 09:10:30 -0700

    add authentication.service

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 <pre>ng new authentication-example</pre>
 <pre>npm install firebase angularfire2 --save</pre>
+<pre>npm install angularfire2@4.0.0-rc.0 firebase --save</pre>
+<pre>npm install promise-polyfill --save-exact</pre>
+<pre>ng g service authentication</pre>
+<pre></pre>
 <pre></pre>
 <pre></pre>
---
+++ src/app/authentication.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthenticationService {
+  user: Observable<firebase.User>;
+
+  constructor(public afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+}
